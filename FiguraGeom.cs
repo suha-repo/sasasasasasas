18982e96a020fa633ebc29afdd874a0d27d79b37
Suhareanu Gabriel <gaby.alex1671@gmail.com>
2020-03-18 20:06:31 +0200

    das

---
+++ FiguraGeom.cs
@@ -0,0 +1,30 @@
+﻿namespace ProiectClase
+{
+    //	Clasa FiguraGeom 
+    public class FiguraGeom
+    {
+        string denumire;
+        int nrLaturi;
+
+        //	Constructor fara parametri
+        public FiguraGeom()
+        {
+            denumire = string.Empty;
+            nrLaturi = 0;
+        }
+
+        //	Constructor cu parametri
+        public FiguraGeom(string _denumire, int _nrLaturi)
+        {
+            denumire = _denumire;
+            nrLaturi = _nrLaturi;
+        }
+
+        //	Metoda care returneaza informatiile despre figura geometrica 
+        //	sub forma unui sir de caractere
+        public string Info()
+        {
+            return string.Format("Figura {0} are {1} laturi", denumire, nrLaturi);
+        }
+    }
+}
